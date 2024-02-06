dcaef522000a511815ac1124db6ff65cf0989cd6
uthmancoder <akandeadebayo025@gmail.com>
2024-02-06 11:35:54 +0100

    initial commit

---
+++ frontend/src/Components/UserAuth/Login.jsx
@@ -0,0 +1,22 @@
+// import React from 'react'
+import {Link} from "react-router-dom"
+
+const Login = () => {
+    return (
+
+
+        <div className="w-full">
+            <h1 className="text-primary text-center fs-2">Log In</h1>
+            <div className="col-10 col-md-8 col-lg-6 mx-auto">
+                <form action="" className=" mx-auto shadow p-4 ml-5" style={{ width: "500px", }}>
+                    <input type="email" placeholder="Enter Email" className="form-control my-2" />
+                    <input type="password" placeholder="Enter Password" className="form-control my-2" />
+                    <p>Don't have an account <Link to="/sign-up">Sign Up</Link></p>
+                    <button type="submit" className="btn btn-primary">Log In</button>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Login
